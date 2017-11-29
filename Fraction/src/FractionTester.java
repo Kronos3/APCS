bf3382f5e99b8788b51495288a1b2f2fd3c722bf
kronos3 <dovakhiin1359@gmail.com>
2017-11-29 16:31:34 -0500

    Fixed Fraction

--- Fraction/src/FractionTester.java
+++ Fraction/src/FractionTester.java
@@ -5,9 +5,10 @@ public class FractionTester
 		Fraction test = new Fraction (2, 3);
 		Fraction test2 = new Fraction (6, 9);
 		
-		System.out.println(test);
-		System.out.println(test2);
-		System.out.println(test2.reduce());
-		System.out.println(test.compareTo(test2));
+		System.out.println (test);
+		System.out.println (test2);
+		System.out.println (test2.reduce ());
+		System.out.println (test.compareTo (test2));
+		System.out.println (test.compareTo (new Fraction (4, 5)));
 	}
 }
