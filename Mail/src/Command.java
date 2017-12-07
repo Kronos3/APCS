1e52ffc6a0678461564c501325e504f27983f634
Kronos3 <dovakhiin1359@gmail.com>
2017-12-07 09:18:30 -0500

    Added javadocs for Mail::Command

--- Mail/src/Command.java
+++ Mail/src/Command.java
@@ -5,6 +5,12 @@ public class Command
 	private String[] args;
 	private Runnable fcall;
 	
+	/**
+	 * Creates a new command given commandName, fcall, and its arguments
+	 * @param commandName the command’s name
+	 * @param fcall the point to the command’s function
+	 * @param args the command’s infinite arguments
+	 */
 	public Command (String commandName, Runnable fcall, String... args)
 	{
 		this.commandName = commandName;
@@ -12,6 +18,10 @@ public class Command
 		this.args = args;
 	}
 	
+	/**
+	 * Prints what you did wrong when creating command
+       @param argc the amount of args inputed from the commandline
+	 */
 	public void printHelp (int argc)
 	{
 		if (argc != -1)
@@ -26,16 +36,28 @@ public class Command
 		System.out.println();
 	}
 	
+	/**
+	 * Returns the pointer to the command's function call
+       @return the pointer to the command's function call
+	 */
 	public Runnable getCall ()
 	{
 		return fcall;
 	}
 	
+	/**
+	 * Returns the count of arguments that this command takes
+     * @return the count of arguments that this command takes
+	 */
 	public int getArgc ()
 	{
 		return this.args.length;
 	}
 	
+	/**
+	 * Returns the string representation of the command
+     * @return the string representation of the command
+	 */
 	public String toString ()
 	{
 		String out = new String();
